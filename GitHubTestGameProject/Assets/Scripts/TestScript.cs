c90123dc2593389619b851b9844392678c653dce
Radjec <radjec@gmail.com>
2016-03-24 14:03:18 -0400

    Test Three

--- GitHubTestGameProject/Assets/Scripts/TestScript.cs
+++ GitHubTestGameProject/Assets/Scripts/TestScript.cs
@@ -7,10 +7,6 @@ public class TestScript : MonoBehaviour
     public bool testBool = true;
 
     public int testInt = 23;
-<<<<<<< HEAD
 
-   
-=======
     public string testString = "Hi Perry!";
->>>>>>> origin/master
 }
